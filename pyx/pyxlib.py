9fe6161779815aeff77336015c767b6babf2c5aa
Pradhyum R <drpradhyum2016@outlook.com>
2022-01-20 19:13:21 +0000

    Fixed expansion issue and made UI better

--- pyx/pyxlib.py
+++ pyx/pyxlib.py
@@ -6,9 +6,9 @@ from subprocess import run
 from spinny import Spinner
 import re
 
-CACHE_DIR = Path('~/.pyx')
+CACHE_DIR = Path.home() / '.pyx'
 if not CACHE_DIR.exists():
-    CACHE_DIR.mkdir()
+    CACHE_DIR.mkdir(parents=True)
 
 class Package:
     def __init__(self, name):
@@ -16,6 +16,8 @@ class Package:
         self.name = re.match(r'\w+', self._name).group(0)
         if not self.installed:
             self.install()
+        else:
+            print(f'\r\033[0;32m✓\033[0m {self.name} already installed')
 
     def __str__(self):
         return f'{phash(self.name)}-{self.name}'
@@ -36,5 +38,5 @@ class Package:
     @staticmethod
     def open_shell_with(ps):
         os.environ['PYTHONPATH'] = ':'.join(str(x.location) for x in ps)
-        os.environ['PS1'] = '(pyx) ' + os.environ['PS1']
+        os.environ['PS1'] = '(\033[31mpyx\033[0m) ' + os.environ['PS1']
         os.system(os.environ['SHELL'])
